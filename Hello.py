71cc3e4d26f4116ad882e177f617905ce5841265
AdityaDolas <dolasadi@gmail.com>
2020-05-03 15:22:51 +0530

    dob and weight

--- Hello.py
+++ Hello.py
@@ -1,3 +1,13 @@
-name = input("what is your name ? ")
-color = input("what is your favorite color ? ")
-print(name +" likes " + color +" color.");
+# name = input("what is your name ? ")
+# color = input("what is your favorite color ? ")
+# print(name +" likes " + color +" color.");
+
+birth_year = input("Birth Year: ")
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(type(age))
+print(age)
+
+weight = input("Weight: ")
+kg = int(weight) * 0.45
+print(kg)
